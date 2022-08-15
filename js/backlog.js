33aa494e30f38178682edb2b1e7fe3813ec3e412
Ilja Gaus-Gerbeth <ilja@Iljas-MacBook-Air.local>
2022-08-15 21:27:53 +0200

    update backlog html

--- js/backlog.js
+++ js/backlog.js
@@ -23,23 +23,26 @@ async function includeHTML() {
     }
 }
 
+/**
+ * This function is used to pull all backlog-tasks from the array allTasks into backlogcards.
+ */
 function render() {
     backlogTasks = document.getElementById('backlogTasks');
     backlogTasks.innerHTML = '';
 
-
-
     for (let i = 0; i < allTasks.length; i++) {
         const backlogTask = allTasks[i];
+        
         backlogTasks.innerHTML += `
     <div class="backlogCard">
+       <div id="tiny-color" class="${backlogTask['category']}"></div>
        <div class="description-width">${backlogTask['assignedTo']}</div>
        <div class="centered">${backlogTask['category']}</div>
        <div class="description-width text-align-right">${backlogTask['description']}</div>
+       <div> <img src="img/trash.png"> </div>
     </div>
     `
     }
 }
-
 
 
