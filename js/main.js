28592333b6a2aab23831725b5818f8567441e208
Rico Denkewitz <rico@MBP-von-Rico.fritz.box>
2022-08-17 21:44:18 +0200

    description in main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 let newUser = [];
 let loggedIn = true;
 
-
+/**
+ * This Function is a simplified spelling for document.getElementById
+ * @param {string} theId is used for the actual Id
+ * @returns document.getElementById();
+ */
 function getId(theId) {
     return document.getElementById(theId);
 }
@@ -14,7 +18,9 @@ function login() {
 
 }
 
-
+/**
+ * This function is used for checking or setting the registration
+ */
 function getRegistrated() {
     let actualUser = getId('userName').value;
     let password = getId('password').value;
@@ -42,20 +48,28 @@ function getRegistrated() {
 
 }
 
-
+/**
+ * This function returns an html part wich is used for an error message
+ * @returns html part
+ */
 function createErrorBox() {
     return /*html*/ `
     <div class = "errorBox"><span><b>This User already exists</b></span></div>
 `
 }
 
-
+/**
+ * This function is for setting an User in Local Storage and as soon as possible in the backend
+ * @param {string} newUser 
+ */
 function saveInLocalStorage(newUser) {
     newUser = JSON.stringify(newUser)
     localStorage.setItem('newUser', newUser);
 }
 
-
+/**
+ * shows the html part for the Registration Section
+ */
 function showRegistrateSection() {
     let registration = getId('registrate');
     getId('loginBtn').style = 'display: none';
@@ -67,23 +81,27 @@ function showRegistrateSection() {
     `
 }
 
-
+/**
+ * This function is for checking if the User is logged In or not so that he can´t evade the Login Section
+ */
 function checkIfUserIsLoggedIn() {
     if (localStorage.getItem("loggedInKey") === null) {
         window.location.href = './index.html'
-    } else {
-        localStorage.setItem('loggedInKey', `${loggedIn}`);
-        window.location.href = './board.html'
     }
 }
 
-
+/**
+ * A simple Guest Login
+ */
 function guestLogin() {
     setDefault();
     window.location.href = './board.html';
 }
 
-
+/**
+ * This function is for setting an default variable to the local storage
+ * and as soon as possible in the backend for The checkIfUserIsLoggedIn Function
+ */
 function setDefault() {
     if (localStorage.getItem("loggedInKey") === null) {
         localStorage.setItem('loggedInKey', loggedIn);
